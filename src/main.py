74a30e3d077435dff165d9d9b1b2891ea04eb6a1
juanfkr <juanferreiraverao@gmail.com>
2026-03-25 14:05:55 -0400

    feat: resolução de caminhos absolutos

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
-import pandas
-from pandas import DataFrame
+#import pandas
+#from pandas import DataFrame
+from pathlib import Path
+from services.pdf_generator_service import PdfGeneratorService
 
-SHEETS_PATH: str = "../shared/sheets"
+#df: DataFrame = pandas.read_csv(f"{SHEETS_PATH}/Amazon-sales.csv")
 
-df: DataFrame = pandas.read_csv(f"{SHEETS_PATH}/Amazon-sales.csv")
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SHEETS_PATH = BASE_DIR / "shared" / "sheets"
+GENERATED_PDF_PATH = BASE_DIR / "shared" / "pdfs"
+
+GENERATED_PDF_PATH.mkdir(parents=True, exist_ok=True)
+
+target_path = str(GENERATED_PDF_PATH / "document.pdf")
+pdf_generator = PdfGeneratorService(target_path)
+
+pdf_generator.generate_pdf("teste")
+
+pdf_generator = PdfGeneratorService(f"{GENERATED_PDF_PATH}/document.pdf");
+
+pdf_generator.generate_pdf("teste")
